641bb4483cc6e584e6a58f518a970e397b90147b
Anthony Martins <128435292+Anthonyysm@users.noreply.github.com>
2026-05-15 01:12:46 -0300

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import { Geist, Geist_Mono, Noto_Sans, Playfair_Display } from "next/font/google"
 
-import "./globals.css"
+import "@/app/globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
 import { cn } from "@/lib/utils";
 
